bd397a6662c67a0a93039021206fe2b4c2302294
transaction-fraud <william.kim102@gmail.com>
2025-10-20 18:13:37 +1100

    Create launch.ps1

---
+++ exe/launch.ps1
@@ -0,0 +1,3 @@
+# ToggleAutotuning by @transaction-fraud
+
+Start-Process powershell.exe -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$PSScriptRoot\toggle.ps1`"" -WindowStyle Hidden
