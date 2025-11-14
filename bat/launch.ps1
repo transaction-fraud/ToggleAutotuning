21ba57f00f3865f6c1cacc1687d5968b3e3c45c2
transaction-fraud <william.kim22@education.nsw.gov.au>
2025-11-14 22:11:43 +1100

    Delete launch.ps1

--- bat/launch.ps1
+++
@@ -1,3 +0,0 @@
-# ToggleAutotuning by @transaction-fraud
-
-Start-Process powershell.exe -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$PSScriptRoot\toggle.ps1`"" -WindowStyle Hidden
